9ebd4a3a4efd1a2b529f0b6172c515cd40404f86
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2025-11-08 15:10:36 +0530

    JaegerExporter added

--- src/httptrigger/Program.cs
+++ src/httptrigger/Program.cs
@@ -2,12 +2,52 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using submit_feedback_http_trigger.Extensions;
 using submit_feedback_http_trigger.Services;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
+// Use environment to decide exporter
+var exporter = Environment.GetEnvironmentVariable("TELEMETRY_EXPORTER") ?? "console";
+
+
 builder.ConfigureFunctionsWebApplication();
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName: "FeedbackApi"))
+    .WithTracing(tracerProviderBuilder =>
+    {
+        tracerProviderBuilder
+            .AddAspNetCoreInstrumentation() // http triggers
+            .AddHttpClientInstrumentation() // Outgoing HTTP calls
+            .SetSampler(new AlwaysOnSampler());
+
+        if (exporter.ToLower() == "console")
+        {
+            tracerProviderBuilder.AddConsoleExporter();
+        }
+        else if (exporter.ToLower() == "jaeger")
+        {
+            tracerProviderBuilder.AddJaegerExporter();
+        }
+        // else if (exporter == "appinsights")
+        // {
+        //     var connString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING");
+        //     if (!string.IsNullOrEmpty(connString))
+        //     {
+        //         tracerProviderBuilder.AddAzureMonitorTraceExporter(options =>
+        //         {
+        //             options.ConnectionString = connString;
+        //         });
+        //     }
+        // }
+        else
+        {
+            tracerProviderBuilder.AddConsoleExporter();
+        }
+    });
 
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
